b125a504aa94dc1b7c54663946eb33d8d2740ad0
Josh Young <j22cal@gmail.com>
2018-06-10 17:31:39 -0500

    Add code to pull in recent repos

---
+++ scripts/api_data.js
@@ -0,0 +1,31 @@
+fetch('https://api.github.com/users/joshayoung/repos')
+.then(function(response) {
+  return response.json();
+})
+.then(function(the_results) {
+  parseIt(the_results)
+});
+
+var parseIt = function(data) {
+  var repos = [];
+  for (var i = 0; i < data.length; i++) {
+    var repo = data[i];
+    repos.push({
+      'url': "<a href='" + repo.html_url + "'>" + repo.name + "</a>",
+      'updated': repo.updated_at
+    });
+  }
+
+  repos.sort(function(a, b) {
+    return new Date(b.updated) - new Date(a.updated)
+  });
+
+  var formatted_data = "";
+  for (var i = 0; i < 5; i++) {
+    repo = repos[i];
+    formatted_data += "<li>" + repo.url + "</li>";
+  }
+
+  document.getElementById('test_github').innerHTML = formatted_data;
+  console.log(repos);
+}
