6120527c683a772bfd6ac32563fd94a4c37a7a2f
Josh Young <j22cal@gmail.com>
2018-11-29 19:12:07 -0600

    Add Tech Spectrum

--- scripts/hide_nav.js
+++ scripts/hide_nav.js
@@ -3,4 +3,16 @@
     document.getElementById('js_main_nav_wrap').className += ' slide-left';
     document.getElementById('main_content').getElementsByTagName('section')[0].style.paddingLeft = 0;
   };
+
+  window.onload = function() {
+    var skillTable = document.querySelectorAll('.skill-spectrum');
+    if (document.querySelectorAll('.skill-spectrum').length > 0) {
+      skillTable[0].onclick = function(e) {
+        if (e.target.tagName == "LI") {
+          e.target.style.backgroundColor = "#000";
+          e.target.style.color = "#fff";
+        }
+      };
+    };
+  };
 }());
