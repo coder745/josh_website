cd98169b5762a87d2e6c67a41d2acef5539bd199
Josh Young <j22cal@gmail.com>
2019-12-09 12:04:09 -0600

    Add feature complete repo list call in api.js

--- data.md
+++ data.md
@@ -8,4 +8,7 @@ weight: 200
 ---
 
 <ul id="github_repos"></ul>
+
+<h3>Feature Complete:</h3>
+<ul id="github_tags"></ul>
 <ul id="wip_commits"></ul>
--- scripts/api.js
+++ scripts/api.js
@@ -1,52 +1,75 @@
 export default class Api {
-  constructor() {
+  constructor(git_hub_repos) {
+    this.git_hub_repos = document.getElementById("github_repos");
+    this.git_hub_tags = document.getElementById("github_tags");
+  }
 
+  getData(url) {
+    let token = window.localStorage.getItem('token');
+    return fetch(url
+      ,{ headers: { 'Authorization': 'token ' + token }, }
+    )
+      .then((response) => {
+        return response.json();
+      });
   }
 
   test() {
-    return 'test';
+    return "test";
   }
 
-  getData(url, callback) {
-    fetch(url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(results) {
-      if(results) {
-        callback(results);
+  addTag(tag, url) {
+    tag.forEach((val) => {
+      if (val.name == 'feature-complete') {
+        this.git_hub_tags.innerHTML += "<li>" + url + "</li>";
+        return;
       }
     });
   }
 
-  repoActivity() {
-    var repos = [];
-    var github_repos = document.getElementById("github_repos");
-    if (typeof(github_repos) != 'undefined' && github_repos != null) {
-      var parseIt = function(data) {
-        for (var i = 0; i < data.length; i++) {
-          var repo = data[i];
-          repos.push({
-            'url': "<a href='" + repo.html_url + "'>" + repo.name + "</a>",
-            'raw_url': repo.url,
-            'updated': repo.updated_at,
-            'language': repo.language,
-            'created_at': repo.created_at.split("T")[0]
-          });
-        }
-
-        repos.sort(function(a, b) {
-          return new Date(b.updated) - new Date(a.updated)
-        });
-
-        var formatted_data = "";
-        for (var i = 0; i < 3; i++) {
-          var repo = repos[i];
-          formatted_data += "<li>" + repo.url + "</li>";
-        }
-        document.getElementById('github_repos').innerHTML = formatted_data;
-      };
-      this.getData('https://api.github.com/users/joshayoung/repos', parseIt)
+  async tags(data) {
+    for (var i = 0; i < data.length; i++) {
+      let results = await this.getData(data[i].tag);
+      this.addTag(results, data[i].url)
     }
   }
-}
+
+  mostRecent(data) {
+    var repos = [];
+    for (var i = 0; i < data.length; i++) {
+      var repo = data[i];
+      repos.push({
+        'url': "<a href='" + repo.html_url + "'>" + repo.name + "</a>",
+        'name': repo.name,
+        'raw_url': repo.url,
+        'tag': repo.url + "/tags",
+        'updated': repo.updated_at,
+        'language': repo.language,
+        'created_at': repo.created_at.split("T")[0]
+      });
+    }
+
+    repos.sort(function(a, b) {
+      return new Date(b.updated) - new Date(a.updated)
+    });
+
+    var formatted_data = "";
+    for (var i = 0; i < 3; i++) {
+      var repo = repos[i];
+      formatted_data += "<li>" + repo.url + "</li>";
+    }
+    document.getElementById('github_repos').innerHTML = formatted_data;
+    this.tags(repos)
+  };
+
+  repoActivity() {
+    var data;
+    if (typeof(this.git_hub_repos) != 'undefined' && this.git_hub_repos != null) {
+      data = this.getData('https://api.github.com/users/joshayoung/repos', this.parseIt)
+    }
+
+    data.then((dta) => {
+      this.mostRecent(dta)
+    });
+  }
+};
